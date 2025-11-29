181392af819a2cf9e2458b86ebe82983708aa65c
hanbee jeong <hb.jade00@gmail.com>
2025-11-29 13:51:32 +0900

    fastapi(2) controller 분리하기

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from typing import Union
+
+#controllers
+from controller import items
+from controller import users
 app = FastAPI()
+app.include_router(items.router)
+app.include_router(users.router)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/item/{item_id}")
-def read_item(item_id: int, q : Union[str, None] = None):
-    return {"item_id" : item_id, "q": q}
